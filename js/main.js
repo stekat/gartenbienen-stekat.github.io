de04f70c46f91c038f016d740741625c4b61294c
stekat <stefandietsch@gmail.com>
2020-06-01 06:42:23 +0200

    Bestand aktualisiert.

--- js/main.js
+++ js/main.js
@@ -23,8 +23,21 @@ function onMessageEvent(messageEvent)
     wassertemperaturWert.textContent = message.Wert + ' °C';
 }
 
+function insertContact() {
+    var c = ['l','a','@','n','h','o','s','.','t','r','e','k','v','g','b','i','-']
+    
+    var hostnameRegexMatch = window.location.hostname.match(/.*?\.(.*)/);
+
+    if (hostnameRegexMatch === null){
+      return;
+    }
+  
+    var textMail = c[4]+c[1]+c[0]+c[0]+c[5]+[16]+[13]+[1]+[9]+[8]+[10]+[3]+[14]+[15]+[10]+[3]+[10]+[3]+c[2]+hostnameRegexMatch[1];
+    document.getElementById("mail").href = "mailto:"+textMail;
+  }
+
 window.onload = function() {
-    // wassertemperaturWert = document.getElementById('wassertemperaturWert');
+    this.insertContact();
 
     loadMoreButton = document.getElementById('load-more');
     loadMoreButton.addEventListener('click', function() {
